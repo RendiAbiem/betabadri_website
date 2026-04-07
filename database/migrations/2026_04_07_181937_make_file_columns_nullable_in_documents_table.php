60dc0e6afdca2197a9d19964451955ad5b742090
Rendi Abiem Pamungkas <rendiabiempamungkas@gmail.com>
2026-04-07 18:21:00 +0700

    add file type documents

---
+++ database/migrations/2026_04_07_181937_make_file_columns_nullable_in_documents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->string('file_path')->nullable()->change();
+            $table->string('file_type')->nullable()->change();
+            $table->integer('file_size')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            //
+        });
+    }
+};
